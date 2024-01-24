1f5ce37ad7d25e89df4c2195d037bf7b7c4cece4
edvard01 <frejaclueless@gmail.com>
2024-01-24 13:35:35 +0100

    Added last few functions after instructions

--- js/index.js
+++ js/index.js
@@ -26,8 +26,62 @@ function getImgData() {
   let width = img.offsetWidth;
   let src = img.getAttribute("src");
 
-  let dataString = `\n{\nurl: ${src}\nwidth: ${width}px\nheight: ${height}px\n}`;
-  return dataString;
+  let dataObject = {
+    height: height,
+    width: width,
+    src: src,
+  };
+  return dataObject;
+}
+
+function getIngredientCount() {
+  const listCount = document.querySelector(
+    ".ingredients-list-paste"
+  ).childElementCount;
+
+  return listCount;
+}
+
+function getPasteListItem(item) {
+  const list = document.querySelector(".ingredients-list-paste").children;
+  console.log(list);
+  return list[item - 1].innerHTML;
+}
+
+function instructionObject() {
+  const instructionList = document.querySelector(".instructions-list").children;
+  let inst1;
+  let inst2;
+  let inst3;
+  let inst4;
+  let inst5;
+  let inst6;
+  let inst7;
+  let inst8;
+  let inst9;
+  let inst10;
+
+  const arr = [
+    inst1,
+    inst2,
+    inst3,
+    inst4,
+    inst5,
+    inst6,
+    inst7,
+    inst8,
+    inst9,
+    inst10,
+  ];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = {
+      number: i + 1,
+      text: instructionList[i].innerHTML,
+    };
+  }
+
+  return arr;
 }
 
 console.log(`The name of the recipe is: ${recipeName()}`);
@@ -37,3 +91,7 @@ console.log(
 );
 console.log(`The attribute of the image is: ${imgAttr()}`);
 console.log("Information about the image:", getImgData());
+console.log(`There are ${getIngredientCount()} ingredients in the paste`);
+console.log(`The 4th item is: ${getPasteListItem(4)}`);
+
+instructionObject();
