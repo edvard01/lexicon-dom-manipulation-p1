acce13f749b369ba20ab29ecba0eb3fc8ce8d3ea
edvard01 <frejaclueless@gmail.com>
2024-01-24 16:09:49 +0100

    Finished fixing the 'Wrong' version through DOM manipulation

--- js/index-p2.js
+++ js/index-p2.js
@@ -29,9 +29,55 @@ function changeImg() {
   img.src = "assets/frozen-cheesecake-slice.jpg";
 }
 
+function changeIngredientsBg() {
+  const element = document.querySelector(".ingredients-container");
+  element.style.backgroundColor = "#f9f9f9";
+}
+
+function fixIngredientList() {
+  const element = document.querySelector(".ingredients-list-bottom");
+  const htmlListItem = "<li>15st digistivetex</li><li>Lite smör</li>";
+  element.innerHTML = htmlListItem;
+}
+
+function changeThirdPasteIng() {
+  const pasteList = document.querySelector(".ingredients-list-paste").children;
+  pasteList[2].innerHTML = "<li>3tsk vaniljsocker</li>";
+}
+
+function addPasteIngredient() {
+  const pasteList = document.querySelector(".ingredients-list-paste");
+  const newElement = document.createElement("li");
+  const newListItemText = document.createTextNode(
+    "400g naturell philadelphiaost"
+  );
+  newElement.appendChild(newListItemText);
+
+  pasteList.insertAdjacentElement("beforeend", newElement);
+}
+
+function removeInstructionShadow() {
+  const instructionTitle = document.querySelector(".instructions");
+  instructionTitle.className = "instructions";
+}
+
+function fixInstructions() {
+  const list = document.querySelector(".instructions-list").children;
+  list[1].innerHTML =
+    "Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.";
+
+  list[8].innerHTML = "Ställ in i frysen över natten.";
+}
+
 changeLogoColor();
 changeHeaderContentPosition();
 changeHeaderBorder();
 changeRecipeName();
 fixTimeContainer();
 changeImg();
+changeIngredientsBg();
+fixIngredientList();
+changeThirdPasteIng();
+addPasteIngredient();
+removeInstructionShadow();
+fixInstructions();
